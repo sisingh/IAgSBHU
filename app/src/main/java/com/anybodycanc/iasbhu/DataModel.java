8c465faec0615925bed53ec54ecc42430ce9da2f
Sid <sid@Sids-MacBook-Pro.local>
2017-10-22 10:41:00 +0530

    first commit

---
+++ app/src/main/java/com/anybodycanc/iasbhu/DataModel.java
@@ -0,0 +1,16 @@
+package com.anybodycanc.iasbhu;
+
+/**
+ * Created by sid on 20/10/17.
+ */
+
+public class DataModel {
+    public int icon;
+    public String name;
+
+    // Constructor.
+    public DataModel(int icon, String name) {
+        this.icon = icon;
+        this.name = name;
+    }
+}
